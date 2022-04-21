f9765795cd24a9182b34494b04979d097cd910e1
Tutov Vladimir <tutovv@gmail.com>
2022-04-21 10:25:01 +0300

    change task server to public

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -15,7 +15,7 @@ const httpOptions = {
   providedIn: 'root'
 })
 export class TaskService {
-  private apiUrl = 'http://localhost:5000/tasks'
+  private apiUrl = 'https://my-json-server.typicode.com/tutovv/angular-crash/tasks'
 
   constructor(private http:HttpClient) { }
 
